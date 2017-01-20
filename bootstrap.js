5f40905df9a29f0a9be432b27b120fc187de0b4a
fbennett <biercenator@gmail.com>
2017-01-20 10:28:04 +0900

    Fix install procedures

--- bootstrap.js
+++ bootstrap.js
@@ -1,44 +1,21 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 var Zotero;
 var oldProcessor;
+var installFlag = false;
 
-/*
- * Zotero runs citeproc-js synchronously within an async thread. We
- * can retrieve modules synchronously inside citeproc-js, and the
- * additional I/O will not impact the UI. Whew.
- */
-
-function ifZotero(succeed, fail) {
-    var ZoteroClass = Cc["@zotero.org/Zotero;1"];
-    if (ZoteroClass) {
-        Zotero = ZoteroClass
-	        .getService(Ci.nsISupports)
-	        .wrappedJSObject;
-        succeed ? succeed(Zotero) : null;
-    } else {
-        fail ? fail() : null;
-    }
-}
-
-function replaceProcessor (Zotero) {
+var installProcessor = function() {
+    Zotero = Cc["@zotero.org/Zotero;1"]
+	    .getService(Ci.nsISupports)
+	    .wrappedJSObject;
     oldProcessor = Zotero.CiteProc.CSL;
     Cu.import("resource://gre/modules/Services.jsm");
     Services.scriptloader.loadSubScript("chrome://propachi/content/citeproc.js", this, "UTF-8");
     Zotero.CiteProc.CSL = CSL;
-}
+}.bind(this);
 
-function UiObserver() {
-    this.register();
-}
-
-UiObserver.prototype = {
+var uiObserver = {
     observe: function(subject, topic, data) {
-        ifZotero(
-            function (Zotero) {
-                replaceProcessor(Zotero);
-            },
-            null
-        );
+        installProcessor();
     },
     register: function() {
         var observerService = Components.classes["@mozilla.org/observer-service;1"]
@@ -51,35 +28,30 @@ UiObserver.prototype = {
         observerService.removeObserver(this, "final-ui-startup");
     }
 }
-var uiObserver = new UiObserver();
-
 
 /*
  * Bootstrap functions
  */
 
 function startup (data, reason) {
-    ifZotero(
-        function (Zotero) {
-            // Set immediately if we have Zotero
-            replaceProcessor(Zotero);
-        },
-        function () {
-            // If not, assume it will arrive by the end of UI startup
-            uiObserver.register();
-        }
-    );
+    if (installFlag) {
+        installProcessor();
+    } else {
+        uiObserver.register();
+    }
 }
 
 function shutdown (data, reason) {
-    uiObserver.unregister();
-    ifZotero(
-        function (Zotero) {
-            Zotero.CiteProc.CSL = oldProcessor;
-        },
-        null
-    );
+    if (installFlag) {
+        Zotero.CiteProc.CSL = oldProcessor;
+        installFlag = false;
+    } else {
+        uiObserver.unregister();
+        Zotero.CiteProc.CSL = oldProcessor;
+    }
 }
 
-function install (data, reason) {}
+function install (data, reason) {
+    installFlag = true;
+}
 function uninstall (data, reason) {}
