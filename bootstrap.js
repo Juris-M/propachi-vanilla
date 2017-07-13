3042f47a4f5286a6e0c2b453eac8574dc17afe2b
bwiernik <bwiernik@users.noreply.github.com>
2017-07-13 15:01:12 +0200

    Add preferences to startup in bootstrap.js
    Doesn't yet have a preferences listener to re-initialize the CSL processor when the setting is changed.

--- bootstrap.js
+++ bootstrap.js
@@ -1,15 +1,61 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
 var Zotero;
 var oldProcessor;
 var installFlag = false;
+
+const PREF_BRANCH = 'extensions.propachi.';
+const PREFS = {
+  uppercase: false
+};
+
+function getPref(key) {
+  // Cache the prefbranch after first use
+  if (getPref.branch == null)
+    getPref.branch = Services.prefs.getBranch(PREF_BRANCH);
+
+  // Figure out what type of pref to fetch
+  switch (typeof PREFS[key]) {
+    case "boolean":
+      return getPref.branch.getBoolPref(key);
+    case "number":
+      return getPref.branch.getIntPref(key);
+    case "string":
+      return getPref.branch.getCharPref(key);
+  }
+}
+
+function setDefaultPrefs() {
+  let branch = Services.prefs.getDefaultBranch(PREF_BRANCH);
+  for (let [key, val] in Iterator(PREFS)) {
+    switch (typeof val) {
+      case "boolean":
+        branch.setBoolPref(key, val);
+        break;
+      case "number":
+        branch.setIntPref(key, val);
+        break;
+      case "string":
+        branch.setCharPref(key, val);
+        break;
+    }
+  }
+}
 
 var installProcessor = function() {
     Zotero = Cc["@zotero.org/Zotero;1"]
 	    .getService(Ci.nsISupports)
 	    .wrappedJSObject;
     oldProcessor = Zotero.CiteProc.CSL;
-    Cu.import("resource://gre/modules/Services.jsm");
-    Services.scriptloader.loadSubScript("chrome://propachi/content/citeproc.js", this, "UTF-8");
+    
+    let UPPERCASE = getPref("uppercase");
+    if(UPPERCASE) {
+      Services.scriptloader.loadSubScript("chrome://propachi-vanilla/content/citeprocUppercase.js", this, "UTF-8");
+    } else {
+      Services.scriptloader.loadSubScript("chrome://propachi-vanilla/content/citeprocVanilla.js", this, "UTF-8");
+    }
+	
     Zotero.CiteProc.CSL = CSL;
 }.bind(this);
 
@@ -34,6 +80,7 @@ var uiObserver = {
  */
 
 function startup (data, reason) {
+    setDefaultPrefs();
     if (installFlag) {
         installProcessor();
     } else {
